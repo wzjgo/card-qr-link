18b7ac44a10135abec5f13499be1387d282fcd63
王知军 <wangzhijun07@meituan.com>
2020-08-20 12:07:43 +0800

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+ 
+app.get('/*', function (req, res) {
+  res.sendFile( __dirname + "/" + "/img/qr-link.png" );
+})
+ 
+var server = app.listen(8080, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
